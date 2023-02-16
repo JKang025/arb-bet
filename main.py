d4cd41b2309b7e046d855acb7dda238120305856
Jeffrey Kang <knightdips@gmail.com>
2023-02-16 03:38:01 -0800

    added email, basic name processing, scheduling

--- main.py
+++ main.py
@@ -1,19 +1,42 @@
-from webscraper import luckbox
-from webscraper import pinnacle
+from webscraper import luckbox, pinnacle
 from calculations import Graph
+from utils import nameProcessing, sendMail
+import schedule
+import time
+
+
 
 
 def main():
+    schedule.every().day.at("12:00").do(script)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(60)
+    
+
+def script():
     luckboxInfo = luckbox()
     luckboxTeamNames = luckboxInfo[0]
+    luckboxTeamNames = nameProcessing(luckboxTeamNames)
     luckboxScores = luckboxInfo[1]
+
     pinnacleInfo = pinnacle()
     pinnacleTeamNames = pinnacleInfo[0]
+    pinnacleTeamNames = nameProcessing(pinnacleTeamNames)
     pinnacleScores = pinnacleInfo[1]
+
+    #testarray = ['Kwangdong Freecs', 'Gen.G', 'UOL Sexy Edition', 'NNO Prime', 'Rebels Gaming', 'Team Heretics Academy', 'BK ROG Esports', 'Team BDS Academy', '3BL Esports', "Team RA'AD", 'MOUZ', 'Eintracht Spandau', 'Cyber Wolves', 'Partizan Esports', 'ZennIT', 'BLX UTD', 'Team GO', 'Izi Dream', 'Fox Gaming', 'Anubis Gaming', 'Guasones Team', 'UCAM Tokiers', 'KRC Genk Esports', 'The Agency', 'GamerLegion', 'BIG', 'Giants Gaming', 'Fnatic TQ', 'Odivelas Sports Club', 'Varona Esports', 'Ankora Gaming', 'Valiance', 'GameWard', 'LDLC OL', 'Nigma Galaxy', 'GnG Esports', 'HEET', 'Northern Lions', 'FC Schalke 04', 'Eintracht Frankfurt']
+    #print(testarray)
+    #print("---------------------------------")
+    #testarray = nameProcessing(testarray)
+    #print(testarray)
+
+
     g = Graph()
     g.initialize_graph("Pinnacle", pinnacleTeamNames, pinnacleScores)
     g.update_graph("Luckbox", luckboxTeamNames, luckboxScores)
-    # g.output_graph()
-
+    g.output_graph()
+      
 if __name__ == '__main__':
 	main()
--- utils.py
+++ utils.py
@@ -1,9 +1,8 @@
 import smtplib
-import ssl
 from email.message import EmailMessage
+import string
 
-
-def sendMail():
+def sendMail(email_message):
     #constants. Replace if not using gmail
     PORT = 465  
     SMTP_SERVER = "smtp.gmail.com"
@@ -19,12 +18,22 @@ def sendMail():
     msg = EmailMessage()
     msg['From'] = SENDER_EMAIL
     msg['To'] = RECIEVER_EMAIL
-
-
     msg['Subject'] = "ARBITRAGE OPPERUNITY"
-    msg.set_content("This is eamil message")
+    msg.set_content(email_message)
 
     with smtplib.SMTP_SSL(SMTP_SERVER, PORT) as smtp:
         smtp.login(SENDER_EMAIL, APP_PASSWORD)
         smtp.send_message(msg)
         
+def nameProcessing(teamname_list):
+    AUTO_REMOVE_WORDS = ["esports", "team", "club", "gaming", "sports"]
+    list = []
+    for name in teamname_list:
+        name = name.lower()
+        name = name.translate(str.maketrans('', '', string.punctuation))
+        for word in AUTO_REMOVE_WORDS:
+            name = name.replace(word, '')
+        name = name.replace("chall", "challengers")
+        name = name.strip()
+        list.append(name)
+    return list   
