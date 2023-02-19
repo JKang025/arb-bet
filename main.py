f77df71f9efa38de66fa57a210f575cd1daf74cf
Jeffrey Kang <knightdips@gmail.com>
2023-02-19 07:05:11 -0800

    bruh

--- main.py
+++ main.py
@@ -19,24 +19,30 @@ def main():
 def script():
     try:
         luckbox_info = luckbox()
-        luckbox_team_name = luckbox_info[0]
+        luckbox_team_name = luckbox_info[1]
         luckbox_team_name = nameStandardize(luckbox_team_name)
-        luckbox_score = luckbox_info[1]
-        luckbox_objs = objectify(luckbox_score, luckbox_team_name, "Luckbox")
+        luckbox_score = luckbox_info[2]
+        luckbox_objs = objectify(luckbox_score, luckbox_team_name, "Luckbox", 0)
 
         pinnacle_info = pinnacle()
-        pinnacle_team_name = pinnacle_info[0]
+        pinnacle_team_name = pinnacle_info[1]
         pinnacle_team_name = nameStandardize(pinnacle_team_name)
-        pinnacle_score = pinnacle_info[1]
-        pinnacle_objs = objectify(pinnacle_score, pinnacle_team_name, "Pinnacle")
-        
-        vulkan_info = vulkan()
-        vulkan_team_names = vulkan_info[0]
-        vulkan_team_names = nameStandardize(vulkan_team_names)
-        vulkan_scores = vulkan_info[1]
-        vulkan_obj = objectify(vulkan_scores, vulkan_team_names, "Vulkan")
+        pinnacle_score = pinnacle_info[2]
+        pinnacle_objs = objectify(pinnacle_score, pinnacle_team_name, "Pinnacle", len(luckbox_objs) + 1)
 
-        joined = luckbox_objs + pinnacle_objs + vulkan_obj
+        vulkan_info = vulkan()
+        vulkan_team_names = vulkan_info[1]
+        vulkan_team_names = nameStandardize(vulkan_team_names)
+        vulkan_scores = vulkan_info[2]
+        vulkan_obj = objectify(vulkan_scores, vulkan_team_names, "Vulkan", len(luckbox_objs) + 1 + len(pinnacle_objs))
+
+        ggbet_info = ggbet()
+        ggbet_team_names = ggbet_info[1]
+        ggbet_team_names = nameStandardize(ggbet_team_names)
+        ggbet_scores = ggbet_info[2]
+        ggbet_obj = objectify(ggbet_scores, ggbet_team_names, "ggbet", len(luckbox_objs) + 1 + len(pinnacle_objs) + len(vulkan_scores))
+
+        joined = luckbox_objs.union(pinnacle_objs).union(vulkan_obj).union(ggbet_obj)
         the_map = mapify(joined)
         list = nameProcessing(the_map)
 
@@ -46,6 +52,8 @@ def script():
         pin_final_score = []
         vulkan_final_name = []
         vulkan_final_score = []
+        ggbet_final_name = []
+        ggbet_final_score = []
         for obj in list:
             if(obj.website == 'Luckbox'):
                 luckbox_final_name.append(obj.name)
@@ -56,12 +64,15 @@ def script():
             elif(obj.website == 'Vulkan'):
                 vulkan_final_name.append(obj.name)
                 vulkan_final_score.append(obj.score)
-
+            elif(obj.website == 'ggbet'):
+                ggbet_final_name.append(obj.name)
+                ggbet_final_score.append(obj.name)
 
         g = Graph()
         g.initialize_graph("Pinnacle", pin_final_name, pin_final_score)
         results = g.update_graph("Luckbox", luckbox_final_name, luckbox_final_score)
         results = results + g.update_graph("Vulkan", vulkan_final_name, vulkan_final_score)
+        results = results + g.update_graph("ggbet", ggbet_final_name, ggbet_final_score)
         if(results != ""):
             sendMail(results)
         else:
