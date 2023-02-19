1a10adecb41e1a4da66b5c5685d83110f0b2d4fb
Jeffrey Kang <knightdips@gmail.com>
2023-02-19 05:16:45 -0800

    implemented main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from webscraper import luckbox, pinnacle
+from webscraper import luckbox, pinnacle, vulkan
 from webscraper import luckbox
 from webscraper import pinnacle
 from webscraper import vulkan
@@ -13,48 +13,54 @@ def main():
     while True:
         schedule.run_pending()
         time.sleep(60)
-    
-    luckboxInfo = luckbox()
-    luckboxTeamNames = luckboxInfo[0]
-    luckboxScores = luckboxInfo[1]
-    pinnacleInfo = pinnacle()
-    pinnacleTeamNames = pinnacleInfo[0]
-    pinnacleScores = pinnacleInfo[1]
-    vulkanInfo = vulkan()
-    vulkanTeamNames = vulkanInfo[0]
-    vulkanScores = vulkanInfo[1]
-
-
-    g = Graph()
-    g.initialize_graph("Pinnacle", pinnacleTeamNames, pinnacleScores)
-    # g.update_graph("Luckbox", luckboxTeamNames, luckboxScores)
-    g.update_graph("Vulkan", vulkanTeamNames, vulkanScores)
-    g.output_graph()
+        
 
 def script():
     try:
-        luckboxInfo = luckbox()
-        luckboxTeamNames = luckboxInfo[0]
-        luckboxTeamNames = nameStandardize(luckboxTeamNames)
-        luckboxScores = luckboxInfo[1]
-        luckbox_objs = objectify(luckboxScores, luckboxTeamNames, "Luckbox")
+        luckbox_info = luckbox()
+        luckbox_team_name = luckbox_info[0]
+        luckbox_team_name = nameStandardize(luckbox_team_name)
+        luckbox_score = luckbox_info[1]
+        luckbox_objs = objectify(luckbox_score, luckbox_team_name, "Luckbox")
 
-
-        pinnacleInfo = pinnacle()
-        pinnacleTeamNames = pinnacleInfo[0]
-        pinnacleTeamNames = nameStandardize(pinnacleTeamNames)
-        pinnacleScores = pinnacleInfo[1]
-        pinnacle_objs = objectify(pinnacleScores, pinnacleTeamNames, "Pinnacle")
+        pinnacle_info = pinnacle()
+        pinnacle_team_name = pinnacle_info[0]
+        pinnacle_team_name = nameStandardize(pinnacle_team_name)
+        pinnacle_score = pinnacle_info[1]
+        pinnacle_objs = objectify(pinnacle_score, pinnacle_team_name, "Pinnacle")
         
-        joined = luckbox_objs + pinnacle_objs
+        vulkan_info = vulkan()
+        vulkan_team_names = vulkan_info[0]
+        vulkan_team_names = nameStandardize(vulkan_team_names)
+        vulkan_scores = vulkan_info[1]
+        vulkan_obj = objectify(vulkan_scores, vulkan_team_names, "Vulkan")
 
+        joined = luckbox_objs + pinnacle_objs + vulkan_obj
         the_map = mapify(joined)
+        list = nameProcessing(the_map)
 
+        luckbox_final_name = []
+        luckbox_final_score = []
+        pin_final_name = []
+        pin_final_score = []
+        vulkan_final_name = []
+        vulkan_final_score = []
+        for obj in list:
+            if(obj.website == 'Luckbox'):
+                luckbox_final_name.append(obj.name)
+                luckbox_final_score.append(obj.score)
+            elif(obj.website == 'Pinnacle'):
+                pin_final_name.append(obj.name)
+                pin_final_score.append(obj.score)
+            elif(obj.website == 'Vulkan'):
+                vulkan_final_name.append(obj.name)
+                vulkan_final_score.append(obj.score)
 
 
         g = Graph()
-        g.initialize_graph("Pinnacle", pinnacleTeamNames, pinnacleScores)
-        results = g.update_graph("Luckbox", luckboxTeamNames, luckboxScores)
+        g.initialize_graph("Pinnacle", pin_final_name, pin_final_score)
+        results = g.update_graph("Luckbox", luckbox_final_name, luckbox_final_score)
+        results = results + g.update_graph("Vulkan", vulkan_final_name, vulkan_final_score)
         if(results != ""):
             sendMail(results)
         else:
